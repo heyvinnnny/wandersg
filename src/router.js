c34ac45223f07dabe0d8515ecb3b3d205ac15892
cwh1999 <80502830+cwh1999@users.noreply.github.com>
2022-03-20 17:07:26 +0800

    Skeleton for Wishlist

--- src/router.js
+++ src/router.js
@@ -12,7 +12,6 @@ import Register from "./views/Register";
 Vue.use(Router);
 
 export default new Router({
-<<<<<<< HEAD
 	routes: [
 		{
 			path: "/",
@@ -49,6 +48,15 @@ export default new Router({
 				footer: { backgroundColor: "black" },
 			},
 		},
+		{
+			path: "/register",
+			name: "register",
+			components: { default: Register, header: MainNavbar, footer: MainFooter },
+			props: {
+				header: { colorOnScroll: 400 },
+				footer: { backgroundColor: "black" },
+			},
+		},
 	],
 	scrollBehavior: (to) => {
 		if (to.hash) {
@@ -57,59 +65,4 @@ export default new Router({
 			return { x: 0, y: 0 };
 		}
 	},
-=======
-  routes: [
-    {
-      path: "/",
-      name: "index",
-      components: { default: Index, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
-      }
-    },
-    {
-      path: "/landing",
-      name: "landing",
-      components: { default: Landing, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
-      }
-    },
-    {
-      path: "/login",
-      name: "login",
-      components: { default: Login, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 }
-      }
-    },
-    {
-      path: "/profile",
-      name: "profile",
-      components: { default: Profile, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
-      }
-    },
-    {
-      path: "/register",
-      name: "register",
-      components: { default: Register, header: MainNavbar, footer: MainFooter },
-      props: {
-        header: { colorOnScroll: 400 },
-        footer: { backgroundColor: "black" }
-      }
-    }
-  ],
-  scrollBehavior: to => {
-    if (to.hash) {
-      return { selector: to.hash };
-    } else {
-      return { x: 0, y: 0 };
-    }
-  }
->>>>>>> 155cdd94753229ee223a931014ac9c64e5bfe4ab
 });
---
+++ src/views/WishList.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- <ImageCard v-for="activity in activities"
+  :key=""></ImageCard> -->
+  
+</template>
+
+<script>
+import ImageCard from './components/ImageCard.vue';
+// import firebaseApp from '../firebase.js';
+// import { getFirestore } from 'firebase/firestore';
+// import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
+// import { getAuth } from 'firebase/auth';
+// const db = getFirestore(firebaseApp)
+export default {
+  components: {
+    ImageCard
+  },
+
+  data() {
+    return {
+      activities: []
+    }
+  }
+  //mounted(){
+  // const auth = getAuth();
+  // either user email or id
+  // this.fbuser = auth.currentUser.email;
+  //}
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>

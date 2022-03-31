753c970cb3afcad3d1fddbb0d292f24ca369d621
ayesaacc <isaachay@gmail.com>
2022-03-31 16:14:53 +0800

    IntermediateProfile.vue page updated with semi-working adventure selection

--- src/views/IntermediateProfile.vue
+++ src/views/IntermediateProfile.vue
@@ -19,7 +19,8 @@
                 </div>
                 <div class="name">
                   <h3 class="title">Continue creating your profile</h3>
-                  <h6>Designer</h6>
+                  <base-alert type="success">You're almost done!</base-alert>
+                  <!-- <h6>Designer</h6>
                   <md-button
                     href="javascript:void(0)"
                     class="md-just-icon md-simple md-dribbble"
@@ -34,68 +35,186 @@
                     href="javascript:void(0)"
                     class="md-just-icon md-simple md-pinterest"
                     ><i class="fab fa-pinterest"></i
-                  ></md-button>
+                  ></md-button> -->
                 </div>
               </div>
             </div>
           </div>
           <div class="description text-center">
-            <p>
-              An artist of considerable range, Chet Faker — the name taken by
-              Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
-              and records all of his own music, giving it a warm, intimate feel
-              with a solid groove structure.
-            </p>
+            <h3 class="title">
+              Please select your preferences for attractions:
+            </h3>
           </div>
-          <div class="profile-tabs">
-            <tabs
-              :tab-name="['Studio', 'Work', 'Favorite']"
-              :tab-icon="['camera', 'palette', 'favorite']"
-              plain
-              nav-pills-icons
-              color-button="success"
-            >
-              <!-- here you can add your content for tab-content -->
-              <template slot="tab-pane-1">
-                <div class="md-layout">
-                  <div class="md-layout-item md-size-25 ml-auto">
-                    <md-button type="button">Hello</md-button>
-                    <img :src="tabPane1[0].image" class="rounded" />
-                    <img :src="tabPane1[1].image" class="rounded" />
-                  </div>
-                  <div class="md-layout-item md-size-25 mr-auto">
-                    <img :src="tabPane1[3].image" class="rounded" />
-                    <img :src="tabPane1[2].image" class="rounded" />
-                  </div>
-                </div>
-              </template>
-              <template slot="tab-pane-2">
-                <div class="md-layout">
-                  <div class="md-layout-item md-size-25 ml-auto">
-                    <img :src="tabPane2[0].image" class="rounded" />
-                    <img :src="tabPane2[1].image" class="rounded" />
-                    <img :src="tabPane2[2].image" class="rounded" />
-                  </div>
-                  <div class="md-layout-item md-size-25 mr-auto">
-                    <img :src="tabPane2[3].image" class="rounded" />
-                    <img :src="tabPane2[4].image" class="rounded" />
-                  </div>
-                </div>
-              </template>
-              <template slot="tab-pane-3">
-                <div class="md-layout">
-                  <div class="md-layout-item md-size-25 ml-auto">
-                    <img :src="tabPane3[0].image" class="rounded" />
-                    <img :src="tabPane3[1].image" class="rounded" />
-                  </div>
-                  <div class="md-layout-item md-size-25 mr-auto">
-                    <img :src="tabPane3[2].image" class="rounded" />
-                    <img :src="tabPane3[3].image" class="rounded" />
-                    <img :src="tabPane3[4].image" class="rounded" />
-                  </div>
-                </div>
-              </template>
-            </tabs>
+          <div>
+            <div class="md-layout md-gutter md-alignment-top-center">
+              <div class="md-layout-item ml-auto">
+                <md-card
+                  md-with-hover
+                  style="width: 320px; margin: 4px; display: inline-block; vertical-align: top;"
+                >
+                  <md-card-media-cover md-text-scrim>
+                    <md-card-media md-ratio="4:3">
+                      <img
+                        :src="tabPane1[0].image"
+                        class="rounded"
+                        style="height:100%;"
+                      />
+                    </md-card-media>
+
+                    <md-card-area>
+                      <md-card-header>
+                        <span class="md-title" style="text-align:center;" id="adventure">
+                          Adventure
+                        </span>
+                      </md-card-header>
+                      <md-card-actions>
+                        <md-button class="adventure" @click="pressed1" v-if="!this.adventure">Select</md-button>
+                        <md-button @click="unpressed1" v-else>Remove</md-button>
+                      </md-card-actions>
+                    </md-card-area>
+                  </md-card-media-cover>
+                </md-card>
+              </div>
+              <div class="md-layout-item ml-auto">
+                <md-card
+                  md-with-hover
+                  style="width: 320px; margin: 4px; display: inline-block; vertical-align: top;"
+                >
+                  <md-card-media-cover md-text-scrim>
+                    <md-card-media md-ratio="4:3">
+                      <img
+                        :src="tabPane1[1].image"
+                        class="rounded"
+                        style="height:100%;"
+                      />
+                    </md-card-media>
+
+                    <md-card-area>
+                      <md-card-header>
+                        <span class="md-title" style="text-align:center;">
+                          Animals
+                        </span>
+                      </md-card-header>
+                      <md-card-actions>
+                        <md-button>Select</md-button>
+                      </md-card-actions>
+                    </md-card-area>
+                  </md-card-media-cover>
+                </md-card>
+              </div>
+              <div class="md-layout-item ml-auto">
+                <md-card
+                  md-with-hover
+                  style="width: 320px; margin: 4px; display: inline-block; vertical-align: top;"
+                  @
+                >
+                  <md-card-media-cover md-text-scrim>
+                    <md-card-media md-ratio="4:3">
+                      <img
+                        :src="tabPane1[2].image"
+                        class="rounded"
+                        style="height:100%;"
+                      />
+                    </md-card-media>
+
+                    <md-card-area>
+                      <!-- <md-card-header style="background-color:rgba(255,255,255,0);"> -->
+                      <span class="md-title" style="text-align:center;">
+                        Children
+                      </span>
+                      <!-- </md-card-header> -->
+                      <md-card-actions>
+                        <md-button md-with-hover>Select</md-button>
+                      </md-card-actions>
+                    </md-card-area>
+                  </md-card-media-cover>
+                </md-card>
+              </div>
+              <div class="md-layout-item ml-auto">
+                <md-card
+                  md-with-hover
+                  style="width: 320px; margin: 4px; display: inline-block; vertical-align: top;"
+                  @
+                >
+                  <md-card-media-cover md-text-scrim>
+                    <md-card-media md-ratio="4:3">
+                      <img
+                        :src="tabPane1[3].image"
+                        class="rounded"
+                        style="height:100%;"
+                      />
+                    </md-card-media>
+
+                    <md-card-area>
+                      <!-- <md-card-header style="background-color:rgba(255,255,255,0);"> -->
+                      <span class="md-title" style="text-align:center;">
+                        Family
+                      </span>
+                      <!-- </md-card-header> -->
+                      <md-card-actions>
+                        <md-button md-with-hover>Select</md-button>
+                      </md-card-actions>
+                    </md-card-area>
+                  </md-card-media-cover>
+                </md-card>
+              </div>
+              <div class="md-layout-item ml-auto">
+                <md-card
+                  md-with-hover
+                  style="width: 320px; margin: 4px; display: inline-block; vertical-align: top;"
+                  @
+                >
+                  <md-card-media-cover md-text-scrim>
+                    <md-card-media md-ratio="4:3">
+                      <img
+                        :src="tabPane1[4].image"
+                        class="rounded"
+                        style="height:100%;"
+                      />
+                    </md-card-media>
+
+                    <md-card-area>
+                      <!-- <md-card-header style="background-color:rgba(255,255,255,0);"> -->
+                      <span class="md-title" style="text-align:center;">
+                        Fantasy
+                      </span>
+                      <!-- </md-card-header> -->
+                      <md-card-actions>
+                        <md-button md-with-hover>Select</md-button>
+                      </md-card-actions>
+                    </md-card-area>
+                  </md-card-media-cover>
+                </md-card>
+              </div>
+              <div class="md-layout-item ml-auto">
+                <md-card
+                  md-with-hover
+                  style="width: 320px; margin: 4px; display: inline-block; vertical-align: top;"
+                  @
+                >
+                  <md-card-media-cover md-text-scrim>
+                    <md-card-media md-ratio="4:3">
+                      <img
+                        :src="tabPane1[5].image"
+                        class="rounded"
+                        style="height:100%;"
+                      />
+                    </md-card-media>
+
+                    <md-card-area>
+                      <!-- <md-card-header style="background-color:rgba(255,255,255,0);"> -->
+                      <span class="md-title" style="text-align:center;">
+                        Food
+                      </span>
+                      <!-- </md-card-header> -->
+                      <md-card-actions>
+                        <md-button md-with-hover>Select</md-button>
+                      </md-card-actions>
+                    </md-card-area>
+                  </md-card-media-cover>
+                </md-card>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -104,37 +223,53 @@
 </template>
 
 <script>
-import { Tabs } from "@/components";
 import { getAuth } from "firebase/auth";
 export default {
   name: "profile2",
-  components: {
-    Tabs
-  },
+  components: {},
   bodyClass: "profile-page",
   data() {
     return {
       tabPane1: [
         { image: require("@/assets/img/preferences/Adventure\ unclicked.jpg") },
-        { image: require("@/assets/img/examples/studio-2.jpg") },
-        { image: require("@/assets/img/examples/studio-4.jpg") },
-        { image: require("@/assets/img/examples/studio-5.jpg") }
+        { image: require("@/assets/img/preferences/Animals\ unclicked.jpg") },
+        { image: require("@/assets/img/preferences/Children\ unclicked.jpg") },
+        { image: require("@/assets/img/preferences/Family\ unclicked.jpg") },
+        { image: require("@/assets/img/preferences/Fantasy\ unclicked.jpg") },
+        { image: require("@/assets/img/preferences/Food\ unclicked.jpg") }
       ],
-      tabPane2: [
-        { image: require("@/assets/img/examples/olu-eletu.jpg") },
-        { image: require("@/assets/img/examples/clem-onojeghuo.jpg") },
-        { image: require("@/assets/img/examples/cynthia-del-rio.jpg") },
-        { image: require("@/assets/img/examples/mariya-georgieva.jpg") },
-        { image: require("@/assets/img/examples/clem-onojegaw.jpg") }
-      ],
-      tabPane3: [
-        { image: require("@/assets/img/examples/mariya-georgieva.jpg") },
-        { image: require("@/assets/img/examples/studio-3.jpg") },
-        { image: require("@/assets/img/examples/clem-onojeghuo.jpg") },
-        { image: require("@/assets/img/examples/olu-eletu.jpg") },
-        { image: require("@/assets/img/examples/studio-1.jpg") }
-      ]
+      email: getAuth().currentUser.email,
+      adventure: false,
+      animals: false,
+      children: false,
+      family: false,
+      fantasy: false,
+      food: false,
+      heritage: false,
+      lifestyle: false,
+      nature: false,
+      romantic: false,
+      scifi: false,
+      staycation: false
     };
+  },
+  methods: {
+    pressed1() {
+      const auth = getAuth();
+      const user = auth.currentUser;
+      if (user) {
+        this.adventure = true;
+        console.log(this.email + "user adventure " + this.adventure);
+      }
+    },
+    unpressed1() {
+      const auth = getAuth();
+      const user = auth.currentUser;
+      if (user) {
+        this.adventure = false;
+        console.log(this.email + " adventure " + this.adventure)
+      }
+    }
   },
   props: {
     header: {
@@ -143,7 +278,7 @@ export default {
     },
     img: {
       type: String,
-      default: require("@/assets/img/faces/christian.jpg")
+      default: require("@/assets/img/blankprofile.webp")
     }
   },
   computed: {
@@ -169,10 +304,6 @@ export default {
   [class*="tab-pane-"] {
     margin-top: 3.213rem;
     padding-bottom: 50px;
-
-    img {
-      margin-bottom: 2.142rem;
-    }
   }
 }
 </style>
